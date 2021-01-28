76acd71d5add6f61e8d32d7ff2565ffc225fb1e7
M. Amine Essadiqi <amessadiqi@gmail.com>
2021-01-29 00:27:01 +0100

    Creating humor features module

---
+++ humor_features/HumorFeatures.py
@@ -0,0 +1,193 @@
+import pandas as pd
+from humor_features.utils import *
+
+
+class HumorFeatures:
+    def __init__(self, dataset = None):
+        self.df = dataset
+        self.df["text"] = self.df["text"].str.replace("!"," !",regex = False)
+        self.df["text"] = self.df["text"].str.replace("?"," ?",regex = False)
+        self.df["text"] = self.df["text"].str.replace("."," .",regex = False)
+        self.df["text"] = self.df["text"].str.replace(","," ,",regex = False)
+        self.df['textSeq'] = self.df["text"].apply(lambda ind:text_to_word_sequence(ind,filters='%\n\t01245679',lower=False, split=' '))
+        self.df['textSeq'] = self.df['textSeq'].apply(lambda ind:[word for word in ind if not word in stopwords.words()])
+        self.df['textSeq'] = self.df['textSeq'].apply(lambda ind:lemmatizeSeq(ind))
+        #self.df['lenSeq'] = self.df["textSeq"].apply(lambda ind: len(ind))
+    
+    ## Structure
+    def getNumWords(self):
+        self.df['nbOfWords'] = self.df["textSeq"].apply(lambda ind:len(np.unique(ind)))
+        return self
+        
+    def getMeanWordLength(self):
+        self.df['meanWordLength'] = self.df["textSeq"].apply(lambda ind:getMeanWordLength(ind))
+        return self
+        
+    def getTags(self):
+        self.df['tags'] = self.df["textSeq"].apply(lambda ind: np.array(pos_tag(ind,tagset='universal')))
+        self.df['tagsNameChange'] = self.df['tags'].apply(lambda tagSeq: np.apply_along_axis(tagRenamer,1,tagSeq) )
+        self.df['tagged'] = self.df["textSeq"].apply(lambda ind: FreqDist(tag for (word,tag) in pos_tag(ind,tagset='universal')))
+        return self
+    
+    def getGrammarRatios(self):
+        self.df[['Adj ratio','Adv ratio','Noun ratio','Verb ratio']] = pd.DataFrame(self.df['tagged'].apply(getRatios).tolist(), index= self.df.index)
+        return self
+    
+    def getPuncCount(self):
+        self.df[['N. commas','N. fullStops','N. exclamation','N. qstMark']] = pd.DataFrame(self.df['textSeq'].apply(getPuncCount).tolist(),index = self.df.index)
+        return self
+    
+    def getEmojiScore(self):
+        self.df["EmojisScore"] = self.df['text'].apply(emojiScorer)
+        return self
+    
+    def getLaughExprCount(self):
+        self.df['laughingExpr'] = self.df['text'].apply(getLaughingExprCounter)
+        return self
+    
+    def getStructure(self):
+        return self.getNumWords().getMeanWordLength().getTags().getGrammarRatios().getPuncCount().getEmojiScore().getLaughExprCount()
+    
+    ## Frequency
+    
+    def getFreqMeanMin(self):
+        self.df[["freqMean","freqMin"]] = pd.DataFrame(self.df['textSeq'].apply(getWordsFreq).tolist(),index = self.df.index)
+        return self
+    
+    def getFreqGap(self):
+        self.df["freqGap"] = self.df['freqMean'] - self.df['freqMin']
+        return self
+    
+    def getFreq(self):
+        return self.getFreqMeanMin().getFreqGap()
+
+    ## Written - Spoken
+    
+    def getSpokenFreqs(self):
+        self.df[["freqSpokenMean", "freqSpokenMin"]] = pd.DataFrame(self.df['textSeq'].apply(getSpokenSeq).tolist(),index = self.df.index)
+        return self
+    
+    def getWrittenFreqs(self):
+        self.df[["freqWrittenMean", "freqWrittenMin"]] = pd.DataFrame(self.df['textSeq'].apply(getWrittenSeq).tolist(),index = self.df.index)
+        return self
+    
+    def getWrittenFreqGap(self):
+        self.df["freqWrittenGap"] = self.df['freqWrittenMean'] - self.df['freqWrittenMin']
+        return self
+
+    
+    def getSpokenFreqGap(self):
+        self.df["freqSpokenGap"] = self.df['freqSpokenMean'] - self.df['freqSpokenMin']
+        return self
+    
+    def getWrittenSpoken(self):
+        return self.getSpokenFreqs().getWrittenFreqs().getWrittenFreqGap().getSpokenFreqGap()
+    
+    ## Synonyms
+    def getSynoLowerGreater(self):
+        self.df[["synoLower","synoGreater","wordLowestSyno","wordGreatestSyno"]] = pd.DataFrame(self.df["textSeq"].apply(getSynoFeaturesSeq).tolist(),index = self.df.index)
+        return self
+    
+    def getSynoGaps(self):
+        self.df["synoLowerGap"] = self.df["wordLowestSyno"] - self.df["synoLower"]
+        self.df["synoGreaterGap"] = self.df["wordGreatestSyno"] - self.df["synoGreater"]
+        return self
+
+    def getSyno(self):
+        return self.getSynoLowerGreater().getSynoGaps()
+    
+    ## Sentiment
+    def getPSentiSum(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiSum' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiSum"] = self.df['posNegObjSenti'].apply(lambda ind:ind.sum(axis=0) )
+        self.df['posSentiSum'] = self.df["posNegObjSentiSum"].apply(lambda ind:ind[0] )
+        return self
+    
+    def getNSentiSum(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiSum' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiSum"] = self.df['posNegObjSenti'].apply(lambda ind:ind.sum(axis=0) )
+        self.df['negSentiSum'] = self.df["posNegObjSentiSum"].apply(lambda ind:ind[1] )
+        return self
+    
+    def getObjSentiSum(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiSum' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiSum"] = self.df['posNegObjSenti'].apply(lambda ind:ind.sum(axis=0) )
+        self.df['objSentiSum'] = self.df["posNegObjSentiSum"].apply(lambda ind:ind[2] )
+        return self
+    
+    def getPSentiMean(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiMean' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiMean"] = self.df['posNegObjSenti'].apply(lambda ind:ind.mean(axis=0) )
+        self.df['posSentiMean'] = self.df["posNegObjSentiMean"].apply(lambda ind:ind[0] )
+        return self
+    
+    def getNSentiMean(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiMean' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiMean"] = self.df['posNegObjSenti'].apply(lambda ind:ind.mean(axis=0) )
+        self.df['negSentiMean'] = self.df["posNegObjSentiMean"].apply(lambda ind:ind[1] )
+        return self
+    
+    def getObjSentiMean(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns or not 'posNegObjSentiMean' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiMean"] = self.df['posNegObjSenti'].apply(lambda ind:ind.mean(axis=0) )
+        self.df['objSentiMean'] = self.df["posNegObjSentiMean"].apply(lambda ind:ind[2] )
+        return self
+    
+    def getPNSentiGap(self):
+        if not 'tags' in self.df.columns:
+            self.getTags()
+        if not 'posNegObjSenti' in self.df.columns and not 'posNegObjSentiSum' in self.df.columns:
+            self.df['posNegObjSenti'] = self.df['tagsNameChange'].apply(lambda tagSeq: np.apply_along_axis(sentimentFeatures,1,tagSeq) )
+            self.df["posNegObjSentiSum"] = self.df['posNegObjSenti'].apply(lambda ind:ind.sum(axis=0) )
+        
+        if not 'posSentiSum' in self.df.columns:
+            getPSentiSum()
+        
+        if not 'negSentiSum' in self.df.columns:
+            getNSentiSum()
+        self.df['posNegGap'] = self.df["posSentiSum"] + self.df["negSentiSum"]
+        return self
+    
+    def getSentiment(self):
+        return self.getPSentiSum().getNSentiSum().getObjSentiSum().getPSentiMean().getNSentiMean().getObjSentiMean().getPNSentiGap()
+
+
+    ## Synsets
+    def getSynsets(self):
+        self.df[["synsetMean","synsetMax","synsetGap"]] = pd.DataFrame(self.df["textSeq"].apply(getSynsetsMeanMaxGap).tolist(),index = self.df.index)
+        return self
+
+
+    """
+    def cleaner(df):
+        df.drop(columns=["text","textSeq"],inplace=True)
+        return df
+    """
+    
+"""
+if __name__=='__main__':
+    features = HumorFeatures()
+
+    f1 = features.lemmatize(sentence, seq=False)
+    f2 = features.lemmatizeWord(sentence)
+
+    f = features.getFeatures(features=['f1', 'f2', 'f3'])
+"""
---
+++ humor_features/utils.pyx
@@ -0,0 +1,277 @@
+from glob import glob
+import json
+from nltk.corpus import wordnet as wn
+from nltk import pos_tag,FreqDist
+from nltk.corpus import sentiwordnet as swn
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+import tensorflow as tf
+from nltk.corpus import stopwords
+from re import search,compile,findall
+from emojis.db import get_emoji_by_alias
+import Cython
+import numpy as np
+import pandas as pd
+
+selectedTags = [ ("haha",3) , ("happy",3) , ("smile",3) , ("pleased",2) , ("laugh",3) , ("joy",3) ,
+                            ("evil",1) , ("devil",1) , ("danger",1) , ("dead",1),("goofy",3), ("wow",2), ("lol",3),
+                            ("perfect",1), ("proud",1), ("praise",1) ]
+wcRatiosDf = pd.read_csv("data/ANC-token-count.csv", sep=',',encoding='ISO-8859-1',header=0).set_index("word")
+writtenDf = pd.read_csv("data/ANC-written-count.csv", sep=',', encoding='ISO-8859-1',header=0)
+writtenDf = writtenDf.set_index("Word")
+writtenDf.drop(columns=["POS","Lemma"],inplace=True)
+writtenDf = writtenDf.groupby("Word").sum()
+spokenDf = pd.read_csv("data/ANC-written-count.csv", sep=',', encoding='ISO-8859-1',header=0)
+spokenDf = spokenDf.set_index("Word")
+spokenDf.drop(columns=["POS","Lemma"],inplace=True)
+spokenDf = spokenDf.groupby("Word").sum()
+totalWrittenWord = writtenDf["Count"].sum()
+totalSpokenWord = spokenDf["Count"].sum()
+lenVectorized = np.vectorize(len)
+
+
+
+cpdef int getMeanWordLength(list array):
+    return int(lenVectorized(array).mean())
+
+cpdef (int,int,int,int) getRatios(freqDict):
+    noun,adj,verb,adv = 0,0,0,0
+    all = 0
+    cdef str tag
+    cdef int occ
+    for (tag,occ) in freqDict.items():
+        if not tag == "'.'":
+            all += occ
+    for (tag,occ) in freqDict.items():
+        if tag == 'NOUN':
+            noun = occ / all
+        if tag == 'ADJ':
+            adj = occ / all
+        if tag == 'VERB':
+            verb = occ / all
+        if tag == 'ADV':
+            adv = occ / all
+    return adj,adv,noun,verb
+
+
+cpdef (int,int,int,int) getPuncCount(list sequence):
+    return np.char.count(sequence,sub=",").sum(),np.char.count(sequence,sub=".").sum(),np.char.count(sequence,sub="!").sum(),np.char.count(sequence,sub="?").sum()
+
+
+cpdef int emojiScorer(sentence):
+    cdef int score = 0
+    for tagArray in [get_emoji_by_alias(alias[1:-1])[2] for alias in compile(':[A-z]*:').findall(sentence)]:
+        for selectedTag in selectedTags:
+            if selectedTag[0] in tagArray:
+                score += selectedTag[1]
+                break
+        continue
+    return score
+
+
+def getLaughingExprCounter(sentence):
+    return len(findall( r"\b(?:a*(?:ha*)+h?|h*ha+h[ha]*|(?:l+o+)+l+|o?l+o+l+[ol]*)\b", sentence))
+
+
+cpdef double getwcRatio( word):
+    if word in wcRatiosDf.index:
+        return float(wcRatiosDf.at[word,"frequency"])
+    else:
+        return np.NaN
+
+
+getwcRatioVectorized = np.vectorize(getwcRatio)
+
+
+cpdef (double,double) getWordsFreq(list sequence):
+    wcRatios = getwcRatioVectorized(sequence)
+    wcRatios = wcRatios[np.logical_not(np.isnan(wcRatios))] 
+    return wcRatios.mean(),wcRatios.min()
+
+cpdef double getWritten( word):
+    if word in writtenDf.index:
+        return float(writtenDf.at[word,"Count"]/totalWrittenWord)
+    else:
+        return np.NaN
+
+
+cpdef double getSpoken(word):
+    if word in spokenDf.index:
+        return float(spokenDf.at[word,"Count"]/totalSpokenWord)
+    else:
+        return np.NaN
+
+
+getWrittenVectorized = np.vectorize(getWritten)
+
+
+getSpokenVectorized = np.vectorize(getSpoken)
+
+
+cpdef (double,double) getWrittenSeq(list sequence):
+    written = getWrittenVectorized(sequence)
+    written = written[np.logical_not(np.isnan(written))] 
+    return written.mean(),written.min()
+
+
+cpdef (double,double) getSpokenSeq(sequence):
+    spoken = getSpokenVectorized(sequence)
+    spoken = spoken[np.logical_not(np.isnan(spoken))] 
+    return spoken.mean(),spoken.min()
+
+
+"""Get synonyms' features"""
+## Split by "." vectorized
+def splitSyno(word):
+    return word.split(".")[0]
+
+
+splitVectorized = np.vectorize(splitSyno)
+
+
+def splitSynos(synoSequence):
+    return splitVectorized(synoSequence)
+
+
+## Filter synonyms vectorized
+def filter(word):
+    if word in wcRatiosDf.index:
+        return word
+
+
+filterVectorized = np.vectorize(filter)
+
+
+def filterSynosSequence(sequence):
+    filteredSeq = filterVectorized(sequence) 
+    return filteredSeq[filteredSeq != 'None'] 
+
+
+## Get frequency vectorized
+def getFreq(word):
+    try:
+        return wcRatiosDf.at[word,"frequency"]
+    except:
+        return -1
+
+
+getFreqVectorized = np.vectorize(getFreq)
+
+
+def getFreqSeq(sequence):
+    frequencies = getFreqVectorized(sequence)
+    return frequencies[frequencies != -1]
+
+
+## Get synsets vectorized
+def getSynsets(word):
+    return wn.synsets(word)
+    
+
+getSynsetsVectorized = np.vectorize(getSynsets)
+
+
+def getSynsetsNumber(word):
+    return len(wn.synsets(word))
+    
+
+getSynsetsNumberVectorized = np.vectorize(getSynsetsNumber)
+
+
+def getSynsetsMeanMaxGap(sequence):
+    try:
+        synNumbers = getSynsetsNumberVectorized(sequence)
+        synsetMean = synNumbers.mean()
+        synsetMax = synNumbers.max()
+        synsetGap = synsetMax - synsetMean
+        return synsetMean,synsetMax,synsetGap
+    except:
+        return 0,0,0
+
+
+def getSynsetsSeq(sequence):
+    return getSynsetsVectorized(sequence)
+
+
+
+def getSynonymFromLemma(lemma):
+    return lemma.name()
+
+
+getSynonymFromLemmaVectorized = np.vectorize(getSynonymFromLemma)
+
+
+def getSynonymFromSeqLemmas(lemmasSequence):
+    return getSynonymFromLemmaVectorized(lemmasSequence)
+
+
+## get synonyms from word vectorized
+def getSynosFromWord(word):
+    synos = filterSynosSequence(splitSynos(getSynonymFromSeqLemmas(getSynsets(word))))
+    return synos,getFreqSeq(synos)
+
+
+## get features vectorized
+def getSynoFeatures(word):
+    try:
+        synonyms,frequencies = getSynosFromWord(word)
+        synoWithFreq = np.array((synonyms,frequencies)).T
+        synoLowerWithFreq = sorted(synoWithFreq, key=lambda x: x[1],reverse = True)
+        synoGreaterWithFreq = sorted(synoWithFreq, key=lambda x: x[1],reverse = False)
+        return len(synoLowerWithFreq[([x for x, y in enumerate(synoLowerWithFreq) if y[0] == word])[0]:]), len(synoGreaterWithFreq[([x for x, y in enumerate(synoGreaterWithFreq) if y[0] == word])[0]:])
+    except:
+        return 0,0
+
+
+getSynoFeaturesVectorized = np.vectorize(getSynoFeatures)
+
+
+def getSynoFeaturesSeq(sequence):
+    try:
+        array = np.array(getSynoFeaturesVectorized(sequence))
+        return np.array(array[0]).mean(),np.array(array[1]).mean(),np.array(array[0]).max(),np.array(array[1]).max()
+    except:
+        return 0,0,0,0
+
+
+# LEMMATIZER
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+
+def lemmatizeWord(word):
+    try:
+        return lemmatizer.lemmatize(word)
+    except:
+        return word
+
+lemmatizeVectorized = np.vectorize(lemmatizeWord)
+def lemmatizeSeq(sequence):
+    try:
+        return lemmatizeVectorized(sequence).tolist()
+    except:
+        return sequence
+
+
+# tag renamer
+def tagRenamer(wordTagTuple):
+    if wordTagTuple[1] == "NOUN":
+        return wordTagTuple[0],'n'
+    
+    elif wordTagTuple[1] == "VERB":
+        return wordTagTuple[0],'v'
+    
+    elif wordTagTuple[1] == "ADJ":
+        return wordTagTuple[0],'a'
+
+    elif wordTagTuple[1] == "ADV":
+        return wordTagTuple[0],'r'
+    else:
+        return wordTagTuple
+
+#Sentiment
+def sentimentFeatures(wordTagTuple):
+    try:
+        mainSyn = next(swn.senti_synsets(wordTagTuple[0],wordTagTuple[1]))
+        return mainSyn.pos_score(),mainSyn.neg_score(),mainSyn.obj_score()
+    except:
+        return 0,0,0
