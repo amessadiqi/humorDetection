f4c9176a2153ae7842cc08c7f637282b1ccd36c1
M. Amine Essadiqi <amessadiqi@gmail.com>
2021-01-29 00:20:57 +0100

    Optimizing data processor

--- data_processing/DataProcessor.py
+++ data_processing/DataProcessor.py
@@ -4,34 +4,15 @@ import pandas as pd
 
 
 class DataProcessor:
-    def __init__(self, data_path):
-        self.path = data_path
-        self.df = self.prepare_data(self.path)
-        self.df = self.clean_data(self.df)
-
-
-    def prepare_data(self, path):
-        all_files = glob.glob(os.path.join(path, "*.csv"))
-        df_from_each_file = (pd.read_csv(f,index_col=0) for f in all_files[:])
-        concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
-
-        return concatenated_df
-
-
-    def clean_data(self, df):
-        df.drop(columns=["text","textSeq"],inplace=True)
-        df["humor"] = df["humor"].astype(int)
-        
-        return df
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.dataset.drop(columns=["text", "textSeq", "tagged", "posNegObjSenti", "posNegObjSentiSum", "posNegObjSentiMean", "tags", "tagsNameChange"],inplace=True)
+        self.dataset["humor"] = self.dataset["humor"].astype(int)
 
 
     def get_processed_df(self):
-        return self.df
+        return self.dataset
 
 
 if __name__=='__main__':
-    PATH = '.'
-    dp = DataProcessor(PATH)
-    df = dp.get_processed_df()
-
-    print(df)
+    pass
